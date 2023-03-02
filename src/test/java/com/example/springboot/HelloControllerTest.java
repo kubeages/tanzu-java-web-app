1d79ec64328feed1d82f03c2f2953181cba5b419
Antonio Gallego <agallego@vmware.com>
2023-03-02 11:01:43 +0100

    v2

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu!", controller.index());
+        assertEquals("Saludos desde el MAPA (app Spring Boot + Tanzu! v2)", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu!"));
+            .andExpect(content().string("Saludos desde el MAPA (app Spring Boot + Tanzu! v2)"));
     }
 }
